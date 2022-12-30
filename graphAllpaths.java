ed35d26c3095ba230eb3c1f57c2f9e611b30acb8
9771-raj <gksagar260@gmail.com>
2022-12-30 18:01:48 +0530

    All paths from src to target

--- graphAllpaths.java
+++ graphAllpaths.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class graphAllpaths {
+import java.util.ArrayList;
+
+public class graphAllpaths {
+    public static void main(String[] args) {
+        int[][] graph={{1,2},{3},{3},{}};
+        ArrayList<ArrayList<Integer>> paths=new ArrayList<>();
+        ArrayList<Integer> arr=new ArrayList<>();
+        arr.add(0);
+        findAllPath(graph,0,paths,arr);
+        System.out.println(paths);
+    }
+
+    private static void findAllPath(int[][] graph, int src, ArrayList<ArrayList<Integer>> paths, ArrayList<Integer> arr) {
+        if(src==graph.length-1){
+            paths.add(new ArrayList<>(arr));
+            return;
+        }
+        for(int i:graph[src]){
+            arr.add(i);
+            findAllPath(graph,i, paths, arr);
+            arr.remove(arr.size()-1);
+        }
+    }
+    
 }
